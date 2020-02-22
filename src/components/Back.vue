262e0b7c97c81842c3c726a7cc05a774f37f00e5
Lukas Zierahn <lukas@kappa-mm.de>
2020-02-22 19:30:15 +0000

    forgot to add files

---
+++ src/components/Back.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navbar">
+    <md-bottom-bar mdType="center">
+      <md-bottom-bar-item to="/Home" md-label="Back" md-icon="home"></md-bottom-bar-item>
+    </md-bottom-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Back',
+}
+</script>
+
+<style>
+</style>
+
---
+++ src/components/Plant.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="d-flex flex-column">
+    <div class="d-flex flex-row">
+      {{plant.name}}
+      <img :src="plant.picture" height="100px" width="100px">
+    </div>
+    <md-progress-bar class="md-accent" md-mode="determinate" :md-value="plant.progress * 100" :height="50"/>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Contact',
+
+  props : ["plant"],
+
+}
+</script>
