113effadcacaac63ccc28d2770e5689f3bfe04f8
Jordan Peters <rollersteaam@googlemail.com>
2020-02-23 03:29:02 +0000

    Remove hello world, fix design prototype

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import HelloWorld from '@/components/HelloWorld'
 import Home from '@/components/Home'
 import PlantList from '@/components/PlantList'
 import About from '@/components/About'
@@ -12,11 +11,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/plantList',
       name: 'PlantList',
