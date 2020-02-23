d35b44531504b2a4ff47fc2f041905f7393d724c
SmartWaterPaul <pw375@exeter.ac.uk>
2020-02-23 03:22:19 +0000

    put home back

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,7 +3,7 @@
     <md-bottom-bar mdType="center">
       <md-bottom-bar-item to="/Social" md-label="Social"><img src="../assets/people.png"/></md-bottom-bar-item>
       <md-bottom-bar-item to="/Learn" md-label="Learn"><img src="../assets/book.png"/></md-bottom-bar-item>
-      <md-bottom-bar-item to="/Projects" md-label="Projects"><img src="../assets/plant.png"></md-bottom-bar-item>
+      <md-bottom-bar-item to="/Home" md-label="Home"><img src="../assets/plant.png"></md-bottom-bar-item>
       <md-bottom-bar-item to="/Cart" md-label="Cart"><img src="../assets/cart.png"></md-bottom-bar-item>
       <md-bottom-bar-item to="/Profile" md-label="Profile"><img src="../assets/person.png"></md-bottom-bar-item>
 
