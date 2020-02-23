4c3031df557de31958da7383bd657b8b8f22315e
Lukas Zierahn <lukas@kappa-mm.de>
2020-02-23 00:55:51 +0000

    added projects

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="navbar">
     <md-bottom-bar mdType="center">
-      <md-bottom-bar-item to="/Home" md-label="Home" md-icon="home"></md-bottom-bar-item>
+      <md-bottom-bar-item to="/Home" md-label="Home"  md-icon="home"></md-bottom-bar-item>
       <md-bottom-bar-item to="/About" md-label="About" md-icon="home"></md-bottom-bar-item>
       <md-bottom-bar-item to="/Contact" md-label="Contact" md-icon="favorite"></md-bottom-bar-item>
     </md-bottom-bar>
@@ -11,6 +11,8 @@
 <script>
 export default {
   name: 'Navigation',
+  components: {
+  }
 }
 </script>
 
