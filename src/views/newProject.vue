ef756d0bf2e1056eff8780d78c0e4f75c40e2f3f
Francisco Terpolilli <frankpablote@mac.com>
2020-02-23 07:38:04 +0000

    Added on newProject

--- src/views/newProject.vue
+++ src/views/newProject.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>Add Project</h1>
+    <div class="div1">
+        <h2>Name</h2>
+        <input type="text" name="Project Name" placeholder="Project Name" />
+
+    </div>
 </div>
 </template>
 
@@ -8,5 +13,9 @@
 </script>
 
 <style>
+.div1{
+    width: 300px;
+    height: 100 px;
+}
 </style>
 
